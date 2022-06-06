5b3148c6d49b7e040a8fc1f708fb6d050181509c
Pontus <52171526+psoder@users.noreply.github.com>
2022-06-06 20:04:05 +0200

    Inital commit

---
+++ server/src/test/kotlin/parkerad/ParkeradApplicationTests.kt
@@ -0,0 +1,13 @@
+package parkerad
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParkeradApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
