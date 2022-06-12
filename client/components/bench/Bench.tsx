7ff0953afa6021a137f3f0d0d22e06f66c33baaf
Pontus <52171526+psoder@users.noreply.github.com>
2022-06-12 11:37:14 +0200

    Continue on frontend

--- client/components/bench/Bench.tsx
+++ client/components/bench/Bench.tsx
@@ -1,37 +1,37 @@
 import Image from "next/image";
 import Link from "next/link";
-import img from "../../public/images/bench.jpg";
 import styles from "./Bench.module.css";
+interface BenchProps {
+  image?: string;
+  location?: [long: number, lat: number, location: string];
+}
 
-const Bench = () => {
-  let x = 18.070502;
-  let y = 59.37815;
-  let coords = `https://maps.google.com/?q=${y},${x}`;
+const Bench = ({
+  image = "/images/bench.jpg",
+  location = [51.59692093054883, 12.584517806189458, "Fippelistan"],
+}: BenchProps) => {
+  let coords = `https://maps.google.com/?q=${location[0]},${location[1]}`;
 
-  https: return (
+  return (
     <div className={styles.review}>
       <div>
         <h2>Review</h2>
         <div className={styles.content}>
           <div>Rating: 420/69</div>
           <div>
-            Location: Fippelistan (
+            Location: {location[2]} (
             <Link href={coords} className={styles.coordinates}>
               View on map
-            </Link>)
+            </Link>
+            )
           </div>
           <div>
-            <h3>User Comments</h3>
-            <p>
-              Nam libero justo laoreet sit amet. Ut porttitor leo a diam. Lorem
-              ipsum dolor sit amet. Faucibus pulvinar elementum integer enim
-              neque volutpat ac tincidunt.
-            </p>
+            Something goes here.
           </div>
         </div>
       </div>
 
-      <Image alt="image location" width={500} height={250} src={img} />
+      <Image alt="image location" width={500} height={250} src={image} />
     </div>
   );
 };
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import Link from 'next/link';
+import Link from "next/link";
+import Bench from "../components/bench/Bench";
+import Footer from "../modules/footer/Footer";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -13,15 +15,37 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Parkerad hos Adam
-        </h1>
-      </main>
+      <section id="intro" className={styles.intro}>
+        <div className={styles.title}>
+          <h1>Parkerad</h1>
+          <h2>A very catchy slogan about benches</h2>
+        </div>
 
-      <footer className={styles.footer}>
-        <h2>Footer</h2>
-      </footer>
+        <Link href="/#benches">
+          <Image
+            src="/icons/arrow-down.png"
+            width={64}
+            height={64}
+            alt="down arrow"
+            layout="fixed"
+            className={styles.downArrow}
+          />
+        </Link>
+      </section>
+
+      <section id="benches" className={styles.benches}>
+        <h1>Bänkar</h1>
+        <div>
+          some feature here
+        </div>
+        <div className={styles.benchList}>
+          <Bench />
+          <Bench />
+          <Bench />
+        </div>
+      </section>
+
+      <Footer />
     </div>
   );
 };
