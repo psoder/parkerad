78f987bd672f7e7265624804503d967b99264e0a
Pontus <52171526+psoder@users.noreply.github.com>
2022-06-13 13:31:37 +0200

    Add development dependencies

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 	kotlin("plugin.spring") version "1.6.21"
 }
 
-group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
@@ -16,10 +15,13 @@ repositories {
 }
 
 dependencies {
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
