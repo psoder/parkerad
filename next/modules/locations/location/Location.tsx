22fc49045e39994792717fbc0b5bff0821ddd1cd
Pontus <52171526+psoder@users.noreply.github.com>
2022-07-07 23:48:05 +0200

    Move to CSS-in-JS and styling.

--- next/modules/locations/location/Location.tsx
+++ next/modules/locations/location/Location.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./Location.module.css";
-import { LocationReview, UserReview } from "types/LocationReview";
+import { LocationReview } from "types/LocationReview";
 import { Review, User } from "@prisma/client";
+import StarBar from "components/starbar/StarBar";
 
 interface LocationProps {
   location: LocationReview;
@@ -10,53 +10,151 @@ interface LocationProps {
 
 const LocationComponent = ({ location }: LocationProps) => {
   const reviews = location.reviews;
-
-  let lat = location.coordinates.coordinates[0];
-  let long = location.coordinates.coordinates[1];
-  let mapLink = `https://maps.google.com/?q=${lat},${long}`;
-
+  const lat = location.coordinates.coordinates[0];
+  const long = location.coordinates.coordinates[1];
+  const mapLink = `https://maps.google.com/?q=${lat},${long}`;
   const avgRating: number =
     reviews
       .map((review) => review.rating)
       .reduce((acc, rating) => acc + rating, 0) / reviews.length;
 
-  return (
-    <article className={styles.location}>
-      <div className={styles.imageContainer}>
+  const LocationImage = () => {
+    return (
+      <div>
         <Image
           src={location.image!}
           alt="image location"
           layout={"fixed"}
-          // objectFit={""}
-          // objectPosition={"cover"}
           width={256}
           height={256}
         />
+
+        <style jsx>
+          {`
+            div {
+              position: relative;
+            }
+          `}
+        </style>
       </div>
+    );
+  };
 
-      <div className={styles.reviews}>
-        <div className={styles.header}>
-          <h2>{location.locationName}</h2>
-          <h2>{avgRating.toFixed(1)} / 5</h2>
-        </div>
+  const LocationHeader = () => {
+    return (
+      <div className="header">
+        <h2>{location.locationName}</h2>
+        {reviews.length == 0 ? (
+          <div>? / 5</div>
+        ) : (
+          <StarBar initalStars={avgRating} size={2} color={"#F5F5F5"} />
+        )}
 
-        <div className={styles.content}>
-          <p className={styles.description}>
-            {location.description ?? "No description available."}
-          </p>
+        <style jsx>
+          {`
+            .header {
+              display: grid;
+              font-size: 2rem;
+              align-items: center;
+              grid-template-columns: 1fr auto;
+              border-bottom: 1px white solid;
+              margin-bottom: 1rem;
+            }
 
-          <Link href={mapLink} className={styles.coordinates}>
+            h2 {
+              margin: 0;
+              font-size: xx-large;
+            }
+          `}
+        </style>
+      </div>
+    );
+  };
+
+  const LocationContent = () => {
+    return (
+      <div className="content">
+        <p className="description">
+          {location.description ?? "No description available."}
+        </p>
+
+        <div className="buttons">
+          <Link href={mapLink} className="coordinates">
             View on map
           </Link>
 
-          <h3>User Reviews ({reviews.length})</h3>
-          <div>
-            {reviews.map((review) => (
-              <Review key={review.id} review={review} user={review.user} />
-            ))}
-          </div>
+          <button>Review</button>
         </div>
+
+        <h3>User Reviews ({reviews.length})</h3>
+        <div>
+          {reviews.map((review) => (
+            <Review key={review.id} review={review} user={review.user} />
+          ))}
+        </div>
+
+        <style jsx>
+          {`
+            .content {
+              display: flex;
+              flex-direction: column;
+              gap: 0.75rem;
+            }
+
+            .description {
+              margin: 0;
+              margin-bottom: 0.5rem;
+            }
+
+            h3 {
+              font-size: 1.5rem;
+            }
+
+            .buttons {
+              display: grid;
+              justify-content: space-between;
+              grid-template-columns: auto auto;
+              align-items: center;
+              font-size: 1.25rem;
+            }
+
+            .buttons > button {
+              background-color: var(--primary);
+              padding: 8px;
+            }
+
+            .buttons > button:hover {
+              background-color: blue;
+            }
+          `}
+        </style>
       </div>
+    );
+  };
+
+  return (
+    <article>
+      <LocationImage />
+
+      <div>
+        <LocationHeader />
+        <LocationContent />
+      </div>
+
+      <style jsx>
+        {`
+          article {
+            background-color: var(--secondary);
+            display: grid;
+            grid-template-columns: auto 1fr;
+            box-shadow: var(--box-shadow);
+          }
+
+          article > div {
+            padding: 1.5rem;
+          }
+        `}
+      </style>
     </article>
   );
 };
@@ -68,10 +166,21 @@ interface ReviewProps {
 
 const Review = ({ review, user }: ReviewProps) => {
   return (
-    <div className={styles.userReview}>
-      Rating: {review.rating}, Comment: &quot;{review.comment}&quot; by{" "}
-      {user.username}
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="review">
+      <p>
+        {review.rating} / 5
+        {review.comment != null && <i>,&quot;{review.comment}&quot;</i>}
+        {" - "} <i>{user.username}</i>
+      </p>
+
+      <style jsx>{`
+        .review {
+          background-color: chocolate;
+          display: flex;
+          padding: 0.25rem;
+          font-size: 1rem;
+        }
+      `}</style>
     </div>
   );
 };
