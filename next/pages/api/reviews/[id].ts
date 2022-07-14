fe3132da3214176a1505e90dd2d25b780b0b4f7d
Pontus <52171526+psoder@users.noreply.github.com>
2022-07-14 18:08:47 +0200

    Logic for creating and reading reviews.

--- next/pages/api/reviews/[id].ts
+++ next/pages/api/reviews/[id].ts
@@ -1,25 +1,41 @@
-import { Prisma, Review } from "@prisma/client";
+import { Review } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "lib/prisma";
 
+type Response = Review | { message: string };
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Review | { message: string }>
+  res: NextApiResponse<Response>
 ) {
-  const id = req.query.id;
+  const { body } = req.body;
+  const id = req.query.id as string;
 
-  let review = await prisma.review
-    .findUnique({
-      where: { id: id as string },
-      include: { location: {}, user: {} },
-    })
-    .catch((reason: any) => {
-      res.status(400).json(reason);
-    });
+  switch (req.method) {
+    case "GET":
+      {
+        const review = await prisma.review
+          .findUnique({
+            where: { id: id as string },
+            include: { location: {}, user: {} },
+          })
+          .catch((reason: any) => {
+            res.status(400).json(reason);
+          });
 
-  if (review == null) {
-    res.status(404).json({ message: `No review with id, '${id}', exists` });
-  } else {
-    res.status(200).json(review);
+        if (review == null) {
+          res
+            .status(404)
+            .json({ message: `No review with id, '${id}', exists` });
+        } else {
+          res.status(200).json(review);
+        }
+      }
+      break;
+
+    default:
+      res.status(405).json({
+        message: `Method, '${req.method}', is not allowed for this endpoint`,
+      });
   }
 }
--- next/pages/api/reviews/create.ts
+++ next/pages/api/reviews/create.ts
@@ -2,13 +2,49 @@ import { Review } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "lib/prisma";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const body = JSON.parse(req.body);
+
   if (req.method !== "POST") {
     res.status(405).send({ message: "Only POST requests allowed" });
     return;
   }
 
-  console.log(req.body)
+  if (
+    await prisma.review.findFirst({
+      where: {
+        userId: body.userId,
+        locationId: body.locationId,
+      },
+    }).catch(() => {console.log("gekl;kl;d");
+    })
+  ) {
+    res.status(200).json({
+      message: `There is already a review with the userId,'${body.userId}', and locationId, '${body.locationId}'.`,
+    });
+    return;
+  }
 
-  res.status(200).json({ response: req.body });
+  await prisma.review.create({
+    data: {
+      userId: body.userId,
+      locationId: body.locationId,
+      rating: +body.rating as number,
+      comment: body.comment,
+    },
+  });
+
+  let rev = await prisma.review.findMany({
+    where: {
+      userId: body.userId as string,
+      locationId: body.locationId as string,
+    },
+  });
+
+  console.log(rev);
+
+  res.status(201).json({ message: "Review created sucessfully" });
 }
