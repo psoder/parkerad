82e88bda293cb2db1de32d2a45f6a1613c1be9bd
Pontus <52171526+psoder@users.noreply.github.com>
2022-07-26 22:47:53 +0200

    Add Semantic-UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
-import { animations, colors, globalCSS } from "theme/Styles";
+import { globalCSS } from "theme/Styles";
 import { SessionProvider } from "next-auth/react";
+import "semantic-ui-css/semantic.min.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
