3b2b8d0536b05d300bfa1dd8f470f10b8fb55b79
Pontus <52171526+psoder@users.noreply.github.com>
2022-07-29 14:01:19 +0200

    Update LocationCard

--- src/components/Location/components/LocationCard.tsx
+++ src/components/Location/components/LocationCard.tsx
@@ -1,91 +1,58 @@
-import StarBar from "components/StarBar";
-import Image from "next/image";
-import { Button } from "semantic-ui-react";
-import { colors, shadows, stdPx } from "theme/Styles";
+import {
+  Button,
+  Card,
+  Container,
+  Header,
+  Image,
+  Rating,
+} from "semantic-ui-react";
 import { LocationReview } from "types/LocationReview";
 import * as utils from "utils/LocationReviewUtils";
 import LocationModal from "./LocationModal";
 
 const LocationCard = ({ location }: { location: LocationReview }) => {
-  const reviews = location.reviews;
-  const titleSize = "2rem";
-
   return (
-    <>
-      <article>
-        <Image
-          src={location.image!}
-          alt="image location"
-          layout={"intrinsic"}
-          width={256}
-          height={256}
-        />
+    <Card image>
+      <Image
+        src={location.image!}
+        alt="Image of location"
+        height={200}
+        style={{
+          objectFit: "cover",
+        }}
+      />
 
-        <div className="content">
-          <h2 className="title">{location.locationName}</h2>
-
-          <div className="rating">
-            {reviews.length == 0 ? (
-              <>? / 5</>
-            ) : (
-              <StarBar
-                initalStars={utils.averageRating(location)}
-                size={"3rem"}
-                style={{ outlineColor: "#FFFF00", fillColor: "#FFFF00" }}
-              />
-            )}
-          </div>
-
-          <LocationModal
-            location={location}
-            trigger={<Button content="More information" fluid />}
+      <Card.Content>
+        <Container
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: 5,
+          }}
+        >
+          <Header
+            style={{
+              margin: 0,
+            }}
+            content={location.locationName}
+            size="large"
           />
-        </div>
-      </article>
+          <Rating
+            rating={utils.averageRating(location)}
+            maxRating={5}
+            disabled
+            icon="star"
+            size="large"
+          />
+        </Container>
 
-      <style jsx>
-        {`
-          * {
-            margin: 0;
-            padding: 0;
-          }
-
-          article {
-            background-color: ${colors.secondary};
-            box-shadow: ${shadows.boxShadow};
-            display: flex;
-            flex-direction: column;
-            width: ${stdPx(25)};
-            color: ${colors.highlight};
-          }
-
-          .content {
-            display: grid;
-            grid-area: content;
-            grid-template-areas:
-              "title rating"
-              "review review";
-            grid-template-rows: auto 3rem;
-            gap: ${stdPx()};
-
-            padding: 5%;
-          }
-
-          .title {
-            grid-area: title;
-            align-self: center;
-            font-size: ${titleSize};
-          }
-
-          .rating {
-            grid-area: rating;
-            align-self: center;
-            justify-self: end;
-            font-size: ${titleSize};
-          }
-        `}
-      </style>
-    </>
+        <LocationModal
+          location={location}
+          trigger={<Button content="More information" fluid />}
+        />
+      </Card.Content>
+    </Card>
   );
 };
 
